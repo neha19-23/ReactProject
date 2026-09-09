192865100ff1aaa872de41877d396670061eeae8
neha19_23 <neha.singh1911994@gmail.com>
2026-09-09 20:36:22 +0530

    Basic start of application and ES6 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState } from 'react-dom/client'
+function App(){
+    // const v = ['mustang','Activa','Honda'];
+    // let [car,bike,cycle] =v;
+    // return (
+    //     <div className="App">
+    //         <h1>Car name {car}. Bike name {bike}. Cycle name {cycle}</h1>
+    //     </div>
+    // );
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [count,setCount] = useState(0);
+    return (
+        <button onClick={()=>setCount(count+1)}>Count : {count}</button>
+    );
 }
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
+//import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { useState } from "react";
 
+import './index.css'
+//  const myelement = (
+//      <table>
+//          <tr>
+//              <th>Name</th>
+//          </tr>
+//          <tr>
+//              <td>Neha</td>
+//          </tr>
+//          <tr>
+//              <td>Sundeep</td>
+//          </tr>
+//
+//      </table>
+//  );
+//
+// const number = [1,2,3,4];
+// const double = number.map(x => x*x);
+
+function Counter(){
+
+    const [count,setCount] = useState(0);
+    return (
+        <div>
+        <button onClick={()=>setCount(count+1)}>Count : {count}</button></div>
+    );
+}
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   // <App/>
+    <Counter/>
 )
