562c89f12f7895c7d7f1e54e5ee8bfc3adc680c2
neha19_23 <neha.singh1911994@gmail.com>
2026-09-14 13:27:35 +0530

    Routing demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import {useState } from 'react'
+import { useState} from 'react'
 import {Expressions} from "./Expressions.jsx";
 import {ShowText} from "./ShowText.jsx";
 import {ColorChange} from "./ClassDemo.jsx";
+import {Home} from "./RoutingDemo.jsx";
+import {BrowserRouter, Route, Link, Routes} from "react-router-dom";
 function App(){
     // const v = ['mustang','Activa','Honda'];
     // let [car,bike,cycle] =v;
@@ -23,14 +25,32 @@ function App(){
     }
     else if(count === 5)
         content = <ColorChange favColor="green" />
-    else if(count === 10)
-        content = alertMessage();
+    else if(count === 10){
+        alertMessage();
+        content=undefined;
+    }
     else
         content = <ShowText name={name}/>
     return (
+        <>
+        <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link> | {" "}
+                <Link to="/classDemo">Color Change</Link> | {" "}
+                <Link to="/expressions">Expression</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/classDemo" element={<ColorChange favColor="Yellow" />}/>
+                <Route path="/expressions" element={<Expressions valv={2} />}/>
+            </Routes>
+        </BrowserRouter>
         <div>
             {content}
         <button onClick={()=>setCount(count+1)}>Count : {count}</button></div>
+        </>
+
+
     );
 
 }
